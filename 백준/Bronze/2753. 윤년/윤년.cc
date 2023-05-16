71ac24d07b001470feb96f7a364f28e3536d33bd
Sechang Choi <95029317+sepang2@users.noreply.github.com>
2023-05-16 16:04:45 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int y;
+  cin >> y;
+
+  if(y % 4 == 0){
+    if(y % 100 != 0 || y % 400 == 0)
+      cout << 1;
+    else
+      cout << 0;
+  }
+  else
+    cout << 0;
+     
+  return 0;
+}
